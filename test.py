8a241cefe9a99d761f8e32a4c76349ef4c2a56af
Raki Delmoro <rakidelmoro@gmail.com>
2025-10-17 11:01:42 +0000

    test model

--- test.py
+++ test.py
@@ -12,11 +12,24 @@ def test_model(model, model_path: str):
     normalized_arr = (image_as_arr - np.min(image_as_arr)) / (np.max(image_as_arr) - np.min(image_as_arr))
 
     # Model prediction
-    prediction = model(torch.tensor(normalized_arr, dtype=torch.float32, device='cuda'))
+    prediction, _ = model(torch.tensor(normalized_arr, dtype=torch.float32, device='cuda'))
     print(f'MODEL: {split[0]} Predicted: {prediction.argmax(-1).item()}')
 
-multi_agents = torch.load('multi_agents.pth')
-standard_mlp = torch.load('mlp.pth')
+def test_multi_agent_model(model, model_path: str):
+    # Load Image
+    split = model_path.split('.')
 
-test_model(multi_agents, 'multi_agents.pth')
-test_model(standard_mlp, 'mlp.pth')
+    img = Image.open('test_image.png')
+    image_as_arr = np.asarray(img).reshape(1, 28*28)
+    # normalize pixel value to range [0, 1]
+    normalized_arr = (image_as_arr - np.min(image_as_arr)) / (np.max(image_as_arr) - np.min(image_as_arr))
+    
+    model.eval()
+    each_agents_prediction = model.test_each_agent(torch.tensor(normalized_arr, dtype=torch.float32, device='cuda'))
+    print(f'MODEL: {split[0]} Predicted: {each_agents_prediction}')
+
+multi_agents = torch.load('multi_agents_v2.pth')
+standard_mlp = torch.load('standard_mlp.pth')
+
+test_multi_agent_model(multi_agents, 'multi_agents_v2.pth')
+test_model(standard_mlp, 'standard_mlp.pth')
