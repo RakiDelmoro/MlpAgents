2af150812c98c04c6648a0a51211a499f3b8b17e
Raki Delmoro <rakidelmoro@gmail.com>
2025-10-28 12:39:50 +0000

    RockV1 runner file

--- runner.py
+++ runner.py
@@ -3,6 +3,7 @@ import pickle
 import torch
 import numpy as np
 from Models.brain import Agent
+from Models.brain_v2 import TopologicalSpatialPooler, SDRUnionClassifier
 
 def mnist_dataloader(img_arr, label_arr, batch_size, shuffle):
     num_samples = img_arr.shape[0]    
@@ -21,22 +22,29 @@ def mnist_dataloader(img_arr, label_arr, batch_size, shuffle):
 def training_runner():
     IMAGE_HEIGHT = 28
     IMAGE_WIDTH = 28
-    DEVICE = 'cuda'
 
     with open('./dataset/mnist.pkl', 'rb') as f: ((train_images, train_labels), (test_images, test_labels), _) = pickle.load(f, encoding='latin1')
     assert train_images.shape[0] == train_labels.shape[0]
     assert test_images.shape[0] == test_labels.shape[0]
     assert train_images.shape[1] == test_images.shape[1] == IMAGE_HEIGHT*IMAGE_WIDTH
 
-    model = Agent(input_size=784, num_neurons=2048)
-    previous_features = None
+    model = Agent(input_size=784, num_neurons=1024)
     for epoch in range(100):
         train_loader = mnist_dataloader(train_images, train_labels, batch_size=1, shuffle=True)
-        test_loader = mnist_dataloader(test_images, test_labels, batch_size=1, shuffle=True)
+        test_loader = mnist_dataloader(test_images[:1000], test_labels, batch_size=1, shuffle=True)
 
-        previous_features = model.phase_1(train_loader, test_loader, epoch, previous_features)
-        # model.reset_memory()
-        # model.phase_2(train_loader, test_loader)
+        # LEARNING...
+        model.phase_1(train_loader)
+
+        # Test model every 5 epochs
+        if (epoch + 1) % 5  == 0:
+            memory_loader = mnist_dataloader(train_images, train_labels, batch_size=1, shuffle=True)
+            model_memories = model.phase_2(memory_loader)
+            accuracy = model.test(model_memories, test_loader)
+
+            print(f'EPOCH: {epoch+1} Accuracy: {accuracy}')
+        else:
+            print(f'EPOCH: {epoch+1} not testing yet...')
 
     torch.save(model, 'RockV1.pth')
 
