8e953a81fc099d2fef2f80d6c36c921ec8e53a76
Raki Delmoro <rakidelmoro@gmail.com>
2025-11-07 11:21:57 +0000

    Improve apical learning

--- Models/brain_v3.py
+++ Models/brain_v3.py
@@ -107,7 +107,7 @@ class CorticalColumn(nn.Module):
     def get_total_params(self):
         return self.basal_synapse.flatten().shape[0] + self.apical_synapse.flatten().shape[0]
     
-    def get_neuron_firing(self, neurons_activation):
+    def get_neurons_firing(self, neurons_activation):
         top_indices = torch.topk(neurons_activation, self.num_active_neurons, dim=-1)[1]
         neurons_firing = torch.zeros_like(neurons_activation)
         neurons_firing.scatter_(-1, top_indices, 1.0)
@@ -124,43 +124,38 @@ class CorticalColumn(nn.Module):
 
         self.basal_synapse.data.clamp_(0.0, 1.0)
 
-    def apical_update(self, apical_features, neurons_activation, neighborhood_size=5):        
-        with torch.no_grad():            
-            # Compute neighborhood strength (cooperative signal)
+    def apical_update(self, apical_features, neurons_activation, neighborhood_size=5):
+        with torch.no_grad():
             neighborhood_strength = torch.zeros_like(self.apical_synapse)
-            
             for i in range(self.apical_size):
                 start = max(0, i - neighborhood_size // 2)
                 end = min(self.apical_size, i + neighborhood_size // 2 + 1)
                 neighborhood_strength[i, :] = self.apical_synapse[start:end, :].mean(dim=0)
 
-            hebbian_term = torch.matmul(apical_features.T, neurons_activation)            
-            # Cooperative modulation: strengthen more if neighbors are strong
-            cooperative_hebbian = hebbian_term * neighborhood_strength
-            output_activity = (neurons_activation ** 2)
-            oja_decay = self.apical_synapse * output_activity
-    
-            self.apical_synapse.data += 0.01 * (cooperative_hebbian - oja_decay)
+            hebbian_term = torch.matmul(apical_features.T, neurons_activation)
+            cooperative_gate = torch.sigmoid(10 * (neighborhood_strength - 0.5))  # branch nonlinear gating
+            oja_decay = self.apical_synapse * (neurons_activation ** 2)
+
+            # Gated Hebbian plasticity modulated by dendritic cooperativity
+            self.apical_synapse.data += 0.01 * ((hebbian_term * cooperative_gate) - oja_decay)
             self.apical_synapse.data.clamp_(0.0, 1.0)
 
     def training_phase(self, image, label):
         basal_features = self.basal_encoder.encode(image)
         apical_features = self.apical_encoder.encode(label)
 
-        basal_activation = torch.matmul(basal_features, self.basal_synapse)
-        apical_activation = torch.matmul(apical_features, self.apical_synapse)
-        neurons_activation = self.get_neuron_firing(basal_activation + apical_activation)
+        neurons_activation = self.get_neurons_firing(torch.matmul(basal_features, self.basal_synapse))
 
         self.basal_update(basal_features, neurons_activation)
         self.apical_update(apical_features, neurons_activation)
 
     def inference_phase(self, image):
         basal_features = self.basal_encoder.encode(image)
-        basal_activation = self.get_neuron_firing(torch.matmul(basal_features, self.basal_synapse))
+        basal_activation = self.get_neurons_firing(torch.matmul(basal_features, self.basal_synapse))
         similarities = []
         for digit in range(self.num_classes):
             apical_features = self.apical_encoder.encode(digit).unsqueeze(0)
-            apical_activation = self.get_neuron_firing(torch.matmul(apical_features, self.apical_synapse))
+            apical_activation = self.get_neurons_firing(torch.matmul(apical_features, self.apical_synapse))
             similarity = torch.nn.functional.cosine_similarity(basal_activation, apical_activation, dim=-1)
             similarities.append(similarity)
         
@@ -171,9 +166,9 @@ class CorticalColumn(nn.Module):
     
     def runner(self, train_loader, test_loader):
         for train_image, train_label in train_loader:
-            train_image = torch.tensor(train_image, device='cuda')
-            train_label = torch.tensor(train_label, device='cuda')
-            self.training_phase(train_image, train_label)
+            image = torch.tensor(train_image, device='cuda')
+            label = torch.tensor(train_label, device='cuda')
+            self.training_phase(image, label)
 
         correctness = []
         for test_image, test_label in test_loader:
